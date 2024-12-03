6be1b8464902fecfa6c1fe192ac67beae14a042a
unknown <dehjr1990@gmail.com>
2024-12-03 18:51:31 -0300

    [INITIAL]

--- app/pages/pag07/script.js
+++ app/pages/pag07/script.js
@@ -1,2 +1,2 @@
-events.on("ready",function(){$(".flip-click").on("click",function(){$(this).addClass("visited"),$(this).removeClass("animated"),$(".flip-click.visited").length>=$(".flip-click").length&&$(".arrow-right").removeClass("hide")})});
+events.on("ready",function(){$(".arrow-right").addClass("inv"),$(".flip-click").on("click",function(){$(this).addClass("visited"),$(this).removeClass("animated"),$(".flip-click.visited").length>=$(".flip-click").length&&$(".arrow-right").removeClass("inv")})});
 //# sourceMappingURL=script.js.map
